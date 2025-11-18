1477b8c2a2743df755a747e9cef20d9240978c5b
RakeshReddy26-bit <rakeshreddyk261@gmail.com>
2025-11-18 22:10:12 +0100

    chore: untrack backups directory to reduce repo bloat (keep local copy)

--- backups/dist-backup-20251103-063706/assets/badge-ruTnR55P.js
+++
@@ -1,2 +0,0 @@
-import{f as t,q as i,o as n,j as o,e as l,c as r}from"./index-BpjOl2gf.js";const d=t(function(a,s){const e=i("Badge",a),{className:g,...c}=n(a);return o.jsx(l.span,{ref:s,className:r("chakra-badge",a.className),...c,__css:{display:"inline-block",whiteSpace:"nowrap",verticalAlign:"middle",...e}})});d.displayName="Badge";export{d as B};
-//# sourceMappingURL=badge-ruTnR55P.js.map
--- backups/rewash-backup-20251103-063638/src/setupTests.ts
+++
@@ -1,2 +0,0 @@
-// Setup for frontend tests with Testing Library and JSDOM
-import '@testing-library/jest-dom';
